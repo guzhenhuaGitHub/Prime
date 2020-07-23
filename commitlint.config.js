c47c466d495f065ebef2afe4d001c6efbe7d3421
顾振华 <guzhenhuab5@163.com>
2020-07-23 11:59:17 +0800

    chore: add commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', [
+            "feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "chore"
+        ]],
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never']
+    }
+}
